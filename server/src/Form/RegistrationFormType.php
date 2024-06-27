f63a290d964b0d214c0762b7311456e86580c469
luci <pelisson.arthur@gmail.com>
2024-06-28 01:15:39 +0200

    fix:form

--- server/src/Form/RegistrationFormType.php
+++ server/src/Form/RegistrationFormType.php
@@ -14,7 +14,7 @@ use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;    
 
 class RegistrationFormType extends AbstractType
 {
